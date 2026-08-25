4c98a5ab01c61095e129940d06df6eaf0dfc35d5
Ken Aida <aidken+github@gmail.com>
2026-08-25 20:10:23 +0900

    minor indenting change to test_project/js/utils.js.

--- test_project/js/utils.js
+++ test_project/js/utils.js
@@ -12,64 +12,64 @@
 
 const Utils = {
 
-/**
- * add two numbers.
- * @param {number} a - a number.
- * @param {number} b - another number
- * @return {number} result, two numbers added.
- */
-add(a, b) {
-  return a + b;
-},
-
-/**
- * subtract b from a.
- * @param {number} a - a number.
- * @param {number} b - number to subtract from a.
- * @return {number} result.
- */
-subtract(a, b) {
-  return a - b;
-},
+  /**
+   * add two numbers.
+   * @param {number} a - a number.
+   * @param {number} b - another number
+   * @return {number} result, two numbers added.
+   */
+  add(a, b) {
+    return a + b;
+  },
 
   /**
-   * capitalize a string.
-   * @param {string} str - a string.
-   * @return {string} capitalized word.
+   * subtract b from a.
+   * @param {number} a - a number.
+   * @param {number} b - number to subtract from a.
+   * @return {number} result.
    */
-capitalize(str) {
-  if (typeof str !== 'string' || str.length === 0) return '';
-  return str.charAt(0).toUpperCase() + str.slice(1);
-},
+  subtract(a, b) {
+    return a - b;
+  },
 
-/**
- * determine if a given number is even.
- * @param {number} n - a number.
- * @return {boolean} true if n is an even number.
- */
-isEven(n) {
-  return n % 2 === 0;
-},
+    /**
+     * capitalize a string.
+     * @param {string} str - a string.
+     * @return {string} capitalized word.
+     */
+  capitalize(str) {
+    if (typeof str !== 'string' || str.length === 0) return '';
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  },
 
-/**
- * returns a reversed version of given string.
- * @param {string} str - input string.
- * @return {string} str - reversed input.
- */
-reverseString(str) {
-  return str.split('').reverse().join('');
-},
+  /**
+   * determine if a given number is even.
+   * @param {number} n - a number.
+   * @return {boolean} true if n is an even number.
+   */
+  isEven(n) {
+    return n % 2 === 0;
+  },
 
-add_2(c) {
-  const a = 5;
-  const b = 2;
+  /**
+   * returns a reversed version of given string.
+   * @param {string} str - input string.
+   * @return {string} str - reversed input.
+   */
+  reverseString(str) {
+    return str.split('').reverse().join('');
+  },
 
-  function double(x) {
-    return x * b
-  };
+  add_2(c) {
+    const a = 5;
+    const b = 2;
 
-  return double(c);
+    function double(x) {
+      return x * b
+    };
 
-},
+    return double(c);
+
+  },
 
 }; // end Utils
